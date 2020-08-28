0c579b965c49442223b1b27b0bbf976b4db76519
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2020-08-28 16:49:26 +1200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module simplefileserver
+
+go 1.15
+
+require golang.org/x/sys v0.0.0-20200826173525-f9321e4c35a6
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/sys/unix"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+)
+
+type Server interface {
+	Hello(w http.ResponseWriter, req *http.Request)
+	Headers(w http.ResponseWriter, req *http.Request)
+	E404(w http.ResponseWriter, req *http.Request)
+	Home(w http.ResponseWriter, req *http.Request)
+	Walk(w http.ResponseWriter, req *http.Request)
+}
+
+type ServerStruct struct {
+	rootDir string
+}
+
+func (s *ServerStruct) Hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "Hello\n")
+}
+
+func (s *ServerStruct) Headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
+func (s *ServerStruct) E404(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "my404\n")
+}
+
+func (s *ServerStruct) Home(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		s.E404(w, req)
+		return
+	}
+	fmt.Fprintf(w, "Home\n")
+}
+
+func (s *ServerStruct) Walk(w http.ResponseWriter, req *http.Request) {
+	requestedFolder := path.Join(strings.Split(req.URL.Path, "/")[2:]...)
+	absPath := path.Join(s.rootDir, requestedFolder)
+
+	fileInfo, err := os.Stat(absPath)
+	if unix.Access(absPath, unix.R_OK) != nil || err != nil || !fileInfo.IsDir() {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "Either the requested directory doesn't exist or access was denied")
+		return
+	}
+	requestedFolder += "/"
+	absPath += "/"
+
+	_, err = fmt.Fprintf(w, "requestedFolder: %s\nabsPath: %s\n\n", requestedFolder, absPath)
+	if err != nil{
+		log.Fatal("Unable to send response")
+	}
+
+	fmt.Fprintf(w, "Listing for dir: %s\n", requestedFolder)
+	files, err := ioutil.ReadDir(absPath)
+	for _, f := range files{
+		name := f.Name()
+		if f.IsDir(){
+			name += "/"
+		}
+		fmt.Fprintf(w, " - %s\n", name)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Requires single argument of root dir\n $ main.go /path/to/dir")
+	}
+	rootDir := os.Args[1]
+
+	fmt.Printf("registered root dir \"%s\"\n", rootDir)
+
+	var newServer Server = &ServerStruct{rootDir: rootDir}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/walk/", newServer.Walk)
+	mux.HandleFunc("/hello", newServer.Hello)
+	mux.HandleFunc("/headers", newServer.Headers)
+	mux.HandleFunc("/", newServer.Home)
+
+	_ = http.ListenAndServe(":8090", mux)
+}
