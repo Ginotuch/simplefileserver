82f83162a14f7e9c5f45f181f73fdc2217ad13d1
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2020-09-01 12:51:07 +1200

    simplified authReqToReq

--- backend/util.go
+++ backend/util.go
@@ -7,29 +7,7 @@ import (
 )
 
 func authReqToReq(authReq *auth.AuthenticatedRequest) *http.Request {
-	return &http.Request{
-		Method:           authReq.Method,
-		URL:              authReq.URL,
-		Proto:            authReq.Proto,
-		ProtoMajor:       authReq.ProtoMajor,
-		ProtoMinor:       authReq.ProtoMinor,
-		Header:           authReq.Header,
-		Body:             authReq.Body,
-		GetBody:          authReq.GetBody,
-		ContentLength:    authReq.ContentLength,
-		TransferEncoding: authReq.TransferEncoding,
-		Close:            authReq.Close,
-		Host:             authReq.Host,
-		Form:             authReq.Form,
-		PostForm:         authReq.PostForm,
-		MultipartForm:    authReq.MultipartForm,
-		Trailer:          authReq.Trailer,
-		RemoteAddr:       authReq.RemoteAddr,
-		RequestURI:       authReq.RequestURI,
-		TLS:              authReq.TLS,
-		Cancel:           authReq.Cancel,
-		Response:         authReq.Response,
-	}
+	return &authReq.Request
 }
 
 func reqToAuthReq(req *http.Request) *auth.AuthenticatedRequest {
